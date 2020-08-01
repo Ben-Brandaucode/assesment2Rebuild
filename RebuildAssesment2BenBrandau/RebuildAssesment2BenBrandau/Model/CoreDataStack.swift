49c589bd36f788c9e532c4c8d2f06b8953fd4c5f
Benjamin Brandau <codingbrandau@gmail.com>
2020-07-31 20:41:09 -0600

    all CoreData Model

---
+++ RebuildAssesment2BenBrandau/RebuildAssesment2BenBrandau/Model/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  RebuildAssesment2BenBrandau
+//
+//  Created by Ben Brandau Brandau on 7/31/20.
+//  Copyright © 2020 Ben Brandau. All rights reserved.
+//
+
+import Foundation
---
+++ RebuildAssesment2BenBrandau/RebuildAssesment2BenBrandau/Model/Item + Convienience.swift
@@ -0,0 +1,9 @@
+//
+//  Item + Convienience.swift
+//  RebuildAssesment2BenBrandau
+//
+//  Created by Ben Brandau Brandau on 7/31/20.
+//  Copyright © 2020 Ben Brandau. All rights reserved.
+//
+
+import Foundation
